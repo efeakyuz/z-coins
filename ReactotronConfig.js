7cc43dcf5ca61bd04f37f8f2e3a8c7b0a1f24293
Efe Akyuz <efenakyuz@gmail.com>
2024-07-21 23:42:01 +0300

    chore: init

---
+++ ReactotronConfig.js
@@ -0,0 +1,20 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import Reactotron, { networking } from 'reactotron-react-native';
+
+Reactotron.setAsyncStorageHandler(AsyncStorage) // AsyncStorage would either come from `react-native` or `@react-native-community/async-storage` depending on where you get it from
+  .configure({
+    name: 'Aposto Debug',
+  })
+  .use(networking())
+  .useReactNative({
+    asyncStorage: false, // there are more options to the async storage.
+    networking: {
+      // optionally, you can turn it off with false.
+      ignoreUrls: /symbolicate/,
+      ignoreUrls: /localhost/,
+    },
+    editor: false, // there are more options to editor
+    errors: { veto: (stackFrame) => false }, // or turn it off with false
+    overlay: false, // just turning off overlay
+  })
+  .connect();
